6544d0059a404c1ffa7bae268f246ab5ee2b0220
Latifah Lillie <queen1892@gmail.com>
2021-03-12 21:17:19 -0600

    Adding A Select Statement to collect values

--- cms.sql
+++ cms.sql
@@ -9,10 +9,23 @@ name_p VARCHAR(30) NULL,
 PRIMARY KEY (id)
 );
 
-CREATE TABLE role(
+CREATE TABLE roles(
 id INT NOT NULL,
 title VARCHAR(30) NULL,
 salary DECIMAL(10,4) NULL,
 department_id INT NULL,
 PRIMARY KEY (id)
 );
+
+CREATE TABLE employee(
+id INT NOT NULL,
+first_name VARCHAR(30) NULL,
+last_name VARCHAR(30) NULL,
+role_id INT NULL,
+manager_id INT NULL,
+PRIMARY KEY (id)
+);
+
+SELECT * FROM department;
+select * from roles;
+select * from employee;
